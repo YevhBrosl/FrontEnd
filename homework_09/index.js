0e6dec34aabd198c65491fe3ff26c2ad82621cf1
Yevheniia <yevheniia79@gmail.com>
2024-04-11 17:54:47 +0200

    new files

--- homework_09/index.js
+++ homework_09/index.js
@@ -1,13 +1,17 @@
-  const btn = document.querySelector('#magic-btn');
+const btn = document.querySelector('#magic-btn');
+const clonedBtn = btn.cloneNode(true);
+clonedBtn.id = 'magic-btn-2';
+clonedBtn.innerText = 'Я изменю тебя';
+clonedBtn.className = 'second-btn';
+//clonedBtn.classList.add("second-btn");
+clonedBtn.style.backgroundColor = "#a78b71"
+clonedBtn.style.color = "white"
 
-  btn.addEventListener('click', () => {
-      const clonedBtn = btn.cloneNode(true);
-      clonedBtn.id = 'magic-btn-2';
-      clonedBtn.innerText = 'Я изменю тебя';
-      clonedBtn.className = 'second-btn';
-      document.body.appendChild(clonedBtn);
-      clonedBtn.addEventListener('click', () => {
-          btn.style.backgroundColor = '#9c4a1a';
-          btn.style.color = 'black';
-      });
-  });
+btn.addEventListener('click', () => {
+  document.body.appendChild(clonedBtn);
+});
+
+clonedBtn.addEventListener('click', () => {
+  btn.style.backgroundColor = '#9c4a1a';
+  btn.style.color = 'black';
+});
