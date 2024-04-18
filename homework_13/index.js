c9e5d967eef580cc58a6b56d7e2875266a962b4c
Yevheniia <yevheniia79@gmail.com>
2024-04-18 16:30:19 +0200

    new files

--- homework_13/index.js
+++ homework_13/index.js
@@ -22,7 +22,7 @@ const calculateTotalAge = (people) => {
     let totalAge = 0
 
     for (let person of people) {
-        const { name, age } = person
+        const { age } = person
         totalAge += person.age
     }
 
@@ -34,25 +34,42 @@ console.log(calculateTotalAge(people))
 // Задание 2
 // Создайте функцию compareObjects, которая принимает два объекта в качестве аргументов и проверяет их на равенство (все ключи и значения должны быть одинаковыми). Верните true, если объекты равны, и false в противном случае.
 
-const user = {
-    name: 'John',
-    age: 25
-}
-
 const user1 = {
     name: 'John',
     age: 25
 }
 
-const compareObjects = (object, object1) => object === object1
-console.log(compareObjects(user, user1))
+const user2 = {
+    name: 'John',
+    age: 25
+}
 
-const compareObjectsByKeys = (object, object1) => object.keys === object1.keys
-console.log(compareObjectsByKeys(user, user1))
+const compareObjects = (object1, object2) => object1 === object2
+console.log(compareObjects(user1, user2))
 
-const compareObjectsByValues = (object, object1) => object.values === object1.values
-console.log(compareObjectsByValues(user, user1))
+const compareObjectsByKeys = (object1, object2) => {
+    let keys1 = Object.keys(object1)
+    let keys2 = Object.keys(object2)
 
+    if (keys1.length !== keys2.length) {
+        return false
+    }
+
+    for (let i = 0; i < keys1.length; i++) {
+        if (keys1[i] !== keys2[i]) {
+            return false
+        }
+    }
+
+    for (let key of keys1) {
+        if (object1[key] !== object2[key]) {
+            return false
+        }
+    }
+
+    return true
+}
+console.log(compareObjectsByKeys(user1, user2))
 
 // Задание 3
 // Напишите функцию findKeyByValue, которая принимает объект и значение в качестве аргументов и возвращает первый ключ, соответствующий переданному значению. Если значение не найдено, верните null.
@@ -76,13 +93,25 @@ const cat = {
 // }
 
 const findKeyByValue = (object, value) => {
-    for (let key in object) {
+    const keys = Object.keys(object);
+
+    for (let key of keys) {
         if (object[key] === value) {
-            return key;
+            return key
         }
     }
-    return null;
+    
+    return null
 }
+
+// const findKeyByValue = (object, value) => {
+//     for (let key in object) {
+//         if (object[key] === value) {
+//             return key;
+//         }
+//     }
+//     return null;
+// }
 
 console.log(findKeyByValue(cat, 5))
 
@@ -97,12 +126,12 @@ const productInfo = {
 
 const printProductDetails = (productInfo) => {
     const data = Object.values(productInfo)
-    let availability
-    if (productInfo.isInStock === true) {
-        availability = 'да'
-    } else {
-        availability = 'нет'
-    }
+    let availability = productInfo.isInStock ? 'да' : 'нет'
+    // if (productInfo.isInStock === true) {
+    //     availability = 'да'
+    // } else {
+    //     availability = 'нет'
+    // }
     let info = `<${data[0]}> - <${data[1]}>, товар на складе: <${availability}>`
     console.log(info)
 }
---
+++ homework_14/index.js
@@ -0,0 +1,100 @@
+// Задание 1
+// У вас есть массив объектов:
+
+// const starWarsHeroes = [
+//     { name: "Anakin Skywalker", age: 30, isJedi: true },
+//     { name: "Luke Skywalker", age: 25, isJedi: true },
+//     { name: "Han Solo", age: 35, isJedi: false },
+//     { name: "Princess Leia", age: 30, isJedi: false },
+//     { name: "Obi-Wan Kenobi", age: 60, isJedi: true },
+// ];
+// Используйте методы массивов
+
+// Создайте на основе старого массива новый массив объектов по образу: [{ name: "Luke Skywalker", isJedi: true }, {name: "Han Solo", isJedi: false}...]
+
+const starWarsHeroes = [
+    { name: "Anakin Skywalker", age: 30, isJedi: true },
+    { name: "Luke Skywalker", age: 25, isJedi: true },
+    { name: "Han Solo", age: 35, isJedi: false },
+    { name: "Princess Leia", age: 30, isJedi: false },
+    { name: "Obi-Wan Kenobi", age: 60, isJedi: true },
+];
+
+const namesAndIsJediObject = starWarsHeroes.map(hero => ({
+    name: hero.name,
+    isJedi: hero.isJedi
+}))
+
+console.log(namesAndIsJediObject)
+
+// Задание 1.2
+// Создайте новый массив с джедаями младше 40 лет
+
+const youngJedi = []
+
+starWarsHeroes.map(hero => {
+    if (hero.age < 40 && hero.isJedi === true) {
+        youngJedi.push(hero)
+    }
+})
+
+console.log(youngJedi)
+
+console.log(starWarsHeroes.filter(hero => hero.age < 40 && hero.isJedi === true))
+
+// Задание 1.3
+// Посчитайте возраст всех джедаев
+
+const totalJediAge = starWarsHeroes
+    .filter(hero => hero.isJedi === true)
+    .reduce((acc, hero) => acc + hero.age, 0)
+
+console.log(totalJediAge)
+
+// Задание 1.4
+// Повысьте возраст героев на 10 лет
+
+starWarsHeroes.forEach(hero => (hero.age += 10))
+console.log(starWarsHeroes)
+
+// Задание 1.5
+// Создайте новый массив, где "Anakin Skywalker" заменен на { name: "Darth Vader", isJedi: false, age: 50 }
+
+const newStarWarsHeroes = []
+
+starWarsHeroes.forEach(hero => (hero.name === "Anakin Skywalker") ? newStarWarsHeroes.push({ name: "Darth Vader", age: 50, isJedi: false }) : newStarWarsHeroes.push(hero))
+
+console.log(newStarWarsHeroes)
+
+// Задание 1.6
+// Создайте массив с именами героев, у которых возраст больше 30 лет, преобразуйте каждое имя в верхний регистр и отфильтруйте имена, которые начинаются с буквы "A".
+// используйте .startsWith()
+
+starWarsHeroes
+    .filter(hero => hero.age > 30)
+    .forEach(hero => (hero.name = hero.name.toUpperCase()))
+
+const heroNamesWithA = starWarsHeroes
+    .filter(hero => hero.name.startsWith("A")) 
+    .map(hero => hero.name)
+
+console.log(heroNamesWithA)
+
+// Задание 1.7
+// Выведите на экран сообщение для каждого героя, указывающее на его статус джедая, и добавьте к каждому сообщению его возраст.
+
+// Используйте тернарный оператор
+
+// Образец:
+
+// 'Anakin Skywalker is a Jedi. Age: 25' 'Han Solo is not a Jedi. Age: 35'
+
+let message = (starWarsHeroes) => {
+    for (let hero of starWarsHeroes) {
+        let jedi = (hero.isJedi === true) ? 'is a Jedi' : 'is not a Jedi'
+        console.log(`${hero.name} ${jedi}. Age: ${hero.age}`)
+    }
+}
+
+message(starWarsHeroes)
+
